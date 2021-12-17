638f887ae66945a9cba38660cffc12fe87bd2ec3
virtu78 <virtu78@yandex.ru>
2021-12-17 14:01:54 +0500

    first commit

---
+++ data/visiologyBrowsers.js
@@ -0,0 +1,153 @@
+var w = {
+  "general": {
+    "renderTo": "c76228affc4d4f46acdbc7e5540db464",
+    "type": "pie",
+    "events": {}
+  },
+  "colors": [
+    "rgba(102,187,106,1.00)",
+    "rgba(255,167,38,1.00)",
+    "rgba(126,87,194,1.00)",
+    "rgba(255,214,0,1.00)",
+    "rgba(66,165,245,1.00)",
+    "rgba(69,69,69,1.00)",
+    "rgba(161,161,161,1.00)",
+    "rgba(217,217,217,1.00)"
+  ],
+  "series": [
+    {
+      "id": "\"Серия 1\"",
+      "name": "Серия 1",
+      "color": "rgba(102,187,106,1.00)",
+      "marker": {
+        "radius": 0.1,
+        "states": {
+          "select": {
+            "radius": 5
+          },
+          "hover": {
+            "radius": 5
+          }
+        }
+      },
+      "data": [
+        {
+          "name": "Chrome",
+          "names": [
+            "Chrome"
+          ],
+          "y": 61.41
+        },
+        {
+          "name": "Internet Explorer",
+          "names": [
+            "Internet Explorer"
+          ],
+          "y": 11.84
+        },
+        {
+          "name": "Firefox",
+          "names": [
+            "Firefox"
+          ],
+          "y": 10.85
+        },
+        {
+          "name": "Edge",
+          "names": [
+            "Edge"
+          ],
+          "y": 4.67
+        },
+        {
+          "name": "Safari",
+          "names": [
+            "Safari"
+          ],
+          "y": 4.18
+        },
+        {
+          "name": "Sogou Explorer",
+          "names": [
+            "Sogou Explorer"
+          ],
+          "y": 1.64
+        },
+        {
+          "name": "Opera",
+          "names": [
+            "Opera"
+          ],
+          "y": 1.6
+        },
+        {
+          "name": "QQ",
+          "names": [
+            "QQ"
+          ],
+          "y": 1.2
+        },
+        {
+          "name": "Other",
+          "names": [
+            "Other"
+          ],
+          "y": 2.61
+        }
+      ]
+    }
+  ],
+  "legend": {
+    "enabled": true,
+    "align": "center",
+    "verticalAlign": "bottom",
+    "layout": "horizontal",
+    "itemStyle": {
+      "align": "center",
+      "color": "rgba(96,125,139,1.00)",
+      "fontFamily": "Open Sans",
+      "fontSize": "12px",
+      "fontWeight": "normal",
+      "fontStyle": "normal"
+    },
+    "itemMarginTop": 1.5
+  },
+  "tooltip": {
+    "style": {
+      "fontFamily": "Open Sans",
+      "fontSize": "12px"
+    }
+  },
+  "plotOptions": {
+    "pie": {
+      "dataLabels": {
+        "style": {
+          "align": "center",
+          "color": "rgba(0,0,0,1)",
+          "fontFamily": "Arial",
+          "fontSize": "14px",
+          "fontWeight": "bold",
+          "fontStyle": "normal",
+          "textOverflow": "none"
+        },
+        "enabled": true,
+        "distance": -12
+      },
+      "size": "100%",
+      "innerSize": "80%",
+      "showInLegend": true,
+      "animation": false
+    }
+  },
+  "drilldown": {
+    "activeDataLabelStyle": {
+      "align": "center",
+      "color": "rgba(0,0,0,1)",
+      "fontFamily": "Arial",
+      "fontSize": "14px",
+      "fontWeight": "bold",
+      "fontStyle": "normal",
+      "textDecoration": "none"
+    }
+  }
+}
